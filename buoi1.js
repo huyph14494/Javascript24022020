05299a7e9bf95a5886f70fc1e9af1f94664dfc32
Sadalu <sadalu900@gmil.com>
2020-02-28 18:32:25 +0700

    type

--- buoi1.js
+++ buoi1.js
@@ -1,7 +1,20 @@
-var a = 10;
+// var a = 10;
 
 
-console.log("Buoi so 1", a);
+// console.log("Buoi so 1", a);
 
-a = "Hello";
-console.log("Buoi so 2", a);
+// a = "Hello";
+// console.log("Buoi so 2", a);
+
+// const arr = [];
+// arr[0] = 10;
+// console.log(arr);
+// arr[0] = 11;
+// console.log(arr);
+
+
+const arr = { "a" : 1 };
+arr["a"] = 10;
+console.log(arr);
+arr["a"] = 11;
+console.log(arr);
